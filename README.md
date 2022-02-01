fac57428f906ad5a170c09732332516317677dd2
Varsha290100 <98559398+Varsha290100@users.noreply.github.com>
2022-01-31 19:17:39 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Audio-Pitch-Shifting
-Pitch shifting of an audio (.wav) file using phase vocoder in python 
+Shifts the pitch of an input wav file...
+
+
+Examples coming
+# Basic Algorithm Flow
+
+```
+Input --> Phase Vocoder (Stretch or compress by 2^(n/12)) --> resample by 2^(n/12)
+```
+
+# Installing
+
+Uses python2.7. Only tested on Linux (Ubuntu 14.04)
+
+```
+$ python setup.py install
+```
+
+This will install pitch-shifter.py
+
+## Requirements
+
+Please see setup.py and requirements.txt files. No additional requirements need to be satisfied.
+
+# Example Usage
+
+The following command 
+```
+$ export PYTHONPATH=.
+$ python ./bin/pitch-shifter.py -s ./samples/sample1.wav -o out.wav -c 4096 -e .9 -p -7 -b .8
+$ totem out.wav
+```
+# References
+
+1. [Guitar Pitch Shifter][grodin1] by François Grondin
+2. [Phase Vocoder Tutorial][dolson1] by Mark Dolson
+3. [NEW PHASE-VOCODER TECHNIQUES FOR PITCH-SHIFTING, HARMONIZING AND OTHER EXOTIC EFFECTS][laroche1] by Laroche and Dolson
+4. [Phase-Vocoder: About this phasing business][laroche2] by Laroche and Dolson
+5. [Phase Locked Vocoder][puckette1] by Puckette
+6. [Improved Phase Vocoder Time-Scale Modification of Audio][laroche3] by Laroche and Dolson
+
+[grodin1]: http://www.guitarpitchshifter.com
+[dolson1]: http://www.eumus.edu.uy/eme/ensenanza/electivas/dsp/presentaciones/PhaseVocoderTutorial.pdf
+[laroche1]: http://labrosa.ee.columbia.edu/~dpwe/papers/LaroD99-pvoc.pdf
+[laroche2]: http://www.ee.columbia.edu/~dpwe/papers/LaroD97-phasiness.pdf
+[puckette1]: http://msp.ucsd.edu/Publications/mohonk95.pdf
+[laroche3]: http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCQQFjAAahUKEwj-gI_gp9HGAhWKPZIKHafnDTM&url=http%3A%2F%2Fwww.cmap.polytechnique.fr%2F~bacry%2FMVA%2Fgetpapers.php%3Ffile%3Dphase_vocoder.pdf%26type%3Dpdf&ei=qx6gVb7_O4r7yASnz7eYAw&usg=AFQjCNFywrNRdVKK9ZhRpQoRjtn5kP4p_A&sig2=wM7GAqHftEI0yB5z4y5i7w&bvm=bv.97653015,d.aWw
---
+++ __init__.py
@@ -0,0 +1,67 @@
+#!python
+
+from pitchshifter import *
+import numpy as np
+
+class TestInPlacePhaseVocoder(object):
+    def test_findPeaks_simple(self):
+        res = findPeaks([1,2,3,2,1])
+        
+        assert len(res) == 1
+        assert res[0] == 2
+
+    def test_findPeaks_earlyPeak(self):
+        res = findPeaks(
+            [8,1,2,4,3,2,1,1])
+
+        print(res)
+        assert len(res) == 2
+        assert res[0] == 0
+        assert res[1] == 3
+    
+    def test_getRegionOfInfluence_3peaks(self):
+        res = getRegionOfInfluence([0,8,21])
+        assert res == [(0,4), (5,14), (15, None)]
+        
+class TestUtilities(object):
+    def test_complex_polarToCartesian_correctResults(self):
+        r = 1.4142     # magnitude
+        theta = 0.7854 # radians
+        results = complex_polarToCartesian(r, theta)
+        
+        expected_results = 1 + 1j
+        assert abs(results.real - expected_results.real) < .01
+        assert abs(expected_results.imag - results.imag) < .01
+
+    def test_complex_cartesianToPolar_correctResults(self):
+        results = complex_cartesianToPolar(1+1j)
+        
+        expected_results = (1.4142, .7854)
+        assert abs(results[0] - expected_results[0]) < .001
+        assert abs(results[1] - expected_results[1]) < .001
+
+    def test_complex_polarToCartesian_arrayCorrectResults(self):
+        rs = np.asarray([1.4142 for i in range(10)])
+        thetas = np.asarray([0.7854 for i in range(10)])
+        
+        results = complex_polarToCartesian(rs, thetas)
+        
+        expected_results = 1 + 1j
+        for result in results:
+            assert abs(result.real - expected_results.real) < .01
+            assert abs(expected_results.imag - result.imag) < .01
+
+    def test_complex_cartesianToPolar_arrayCorrectResults(self):
+        rs, thetas= complex_cartesianToPolar(np.asarray([1+1j for i in range(10)]))
+
+        expected_results = (1.4142, .7854)
+        for r in rs:
+            assert abs(r - expected_results[0]) < .001
+
+        for theta in thetas:
+            assert abs(theta - expected_results[1]) < .001
+    
+#class TestSTFT(object):
+#    def test_stft_correct_results(self):
+#        assert False
+
